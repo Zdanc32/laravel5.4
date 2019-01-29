6aab0f3433468bea3c762467f1faa61d93bbb697
Gabriel <zdanc322@gmail.com>
2019-01-29 21:30:10 +0100

    add try catch section and transactions to create and update

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -7,8 +7,10 @@ use App\Price;
 use App\Product;
 use Cron\Tests\AbstractFieldTest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use MongoDB\BSON\Regex;
+
 
 class StoreController extends Controller
 {
@@ -23,13 +25,22 @@ class StoreController extends Controller
     }
     public function store(Request $request)
     {
-        $product = new Product([
-            'product_name' => $request->get('product_name'),
-            'product_description' => $request->get('description')
-        ]);
-        $product->save();
-        $prices = explode(',', $request->get('prices'));
-        $this->insert_prices_to_product($product, $prices);
+        DB::beginTransaction();
+        try
+        {
+            $product = new Product([
+                'product_name' => $request->get('product_name'),
+                'product_description' => $request->get('description')
+            ]);
+            $product->save();
+            $prices = explode(',', $request->get('prices'));
+            $this->insert_prices_to_product($product, $prices);
+        }
+        catch (\Exception $e)
+        {
+            DB::rollBack();
+        }
+        DB::commit();
         return redirect('/products');
     }
     public function edit(Product $product)
@@ -38,21 +49,28 @@ class StoreController extends Controller
     }
     public function update(Request $request, Product $product)
     {
-        $product->product_name = $request->get('product_name');
-        $product->product_description = $request->get('description');
-        $product->save();
-        $prices = Price::where('product_dwfk', $product->product_dwid)->get();
-
-        foreach ($prices as $price)
+        DB::beginTransaction();
+        try
         {
-            $price->delete();
+            $product->product_name = $request->get('product_name');
+            $product->product_description = $request->get('description');
+            $product->save();
+            $prices = Price::where('product_dwfk', $product->product_dwid)->get();
+
+            foreach ($prices as $price) {
+                $price->delete();
+            }
+
+            $new_prices = explode(',', $request->get('prices'));
+            $this->insert_prices_to_product($product, $new_prices);
         }
-
-        $new_prices = explode(',', $request->get('prices'));
-
-        $this->insert_prices_to_product($product, $new_prices);
-
+        catch (\Exception $e)
+        {
+            DB::rollBack();
+        }
+        DB::commit();
         return redirect('/products');
+
     }
     public function destroy(Product $product)
     {
